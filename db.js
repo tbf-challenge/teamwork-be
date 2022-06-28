62a1001ab1c317b342a0ab06fdf3fcecc0785170
jakazzy <jakazzy@gmail.com>
2022-06-28 05:41:27 +0000

    setup(x): setup the be for the project

---
+++ db.js
@@ -0,0 +1,16 @@
+const pgtools = require('pgtools')
+
+const config = {
+    user: '',
+    password: '',
+    port: '',
+    host: ''
+}
+
+pgtools.createdb(config, 'test-db', (err, res)=> {
+    if(err){
+        console.log(err);
+        process.exit(-1)
+    }
+    console.log(res)
+})
