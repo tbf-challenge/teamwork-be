1db9db6075f275c10788e2053628dba9a68369ee
jakazzy <jakazzy@gmail.com>
2022-08-08 05:49:52 +0000

    feat(x): restructure project

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const router = require('./routes')
+
+const app = express()
+
+// Middleware
+app.use(express.json())
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+app.get('/', (req, res) => {
+    res.send('Hello friend')
+})
+
+// Routes
+app.use('api/v1', router)
+
+module.exports = app
