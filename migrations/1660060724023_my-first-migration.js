40472948678ded033174c66f1d17002bb9bbad83
jakazzy <jakazzy@gmail.com>
2022-08-09 18:28:10 +0000

    feat(x): add script for migrations

---
+++ migrations/1660060724023_my-first-migration.js
@@ -0,0 +1,92 @@
+/* eslint-disable no-undef */
+/* eslint-disable camelcase */
+
+exports.shorthands = { 
+    id: { type: 'uuid', primaryKey: true }, 
+    createdAt: {
+    type: 'timestamp',
+    notNull: true,
+    default: new PgLiteral('current_timestamp') ,
+  }, 
+    updatedAt: {
+    type: 'timestamp',
+    notNull: true,
+    default: new PgLiteral('current_timestamp on updated current_timestamp') ,
+  }, 
+};
+
+exports.up = (pgm) => {
+    pgm.createTable('users', {
+      id: 'id',
+      firstName: { type: 'varchar(1000)', notNull: true },
+      lastName: { type: 'varchar(1000)', notNull: true },
+      email: { type: 'varchar(100)', notNull: true },
+      passwordHash: { type: 'varchar(100)', notNull: true },
+      gender: { type: 'varchar(100)', notNull: true },
+      jobRole: { type: 'varchar(100)' },
+      department: { type: 'varchar(100)', notNull: true },
+      address: { type: 'varchar(1000)', notNull: true },
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt',
+
+    })
+    pgm.createTable('posts', {
+      id: 'id',
+      userId: {
+        type: 'uuid',
+        notNull: true,
+        references: '"users"',
+        onDelete: 'cascade',
+      },
+      title: { type: 'text', notNull: true },
+      image: { type: 'text' },
+      content: { type: 'text', notNull: true },
+      published: { type: 'boolean' },
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt',
+    })
+    pgm.createTable('comments', {
+      id: 'id',
+      userId: {
+        type: 'uuid',
+        notNull: true,
+        references: '"users"',
+        onDelete: 'cascade',
+      },
+      postId: {
+        type: 'integer',
+        notNull: true,
+        references: '"posts"',
+        onDelete: 'cascade',
+      },
+      content: { type: 'text', notNull: true },
+      published: { type: 'boolean' },
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt',
+    })
+    pgm.createTable('tags', {
+      id: 'id',
+      content: { type: 'text', notNull: true },
+      title: { type: 'text', notNull: true },
+    })
+    pgm.createTable('posts_tags', {
+    postId: {
+        type: 'integer',
+        notNull: true,
+        references: '"posts"',
+        onDelete: 'cascade',
+        },
+    tagId: {
+        type: 'integer',
+        notNull: true,
+        references: '"tags"',
+        onUpdate: 'cascade',
+        onDelete: 'cascade',
+        },    
+      content: { type: 'text', notNull: true },
+      title: { type: 'text', notNull: true },
+    })
+    pgm.createIndex('posts', 'userId')
+  }
+
+  exports.down = false
