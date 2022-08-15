7d2d64e59ba0b0b404ecec66a4f27c3c95178c35
jakazzy <jakazzy@gmail.com>
2022-08-15 18:22:42 +0000

    add passportjs

--- src/app.js
+++ src/app.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors');
+const passport = require('passport')
 
+const passportLocal = require('./lib/passport-local')
+const passportJwt = require('./lib/passport-jwt')
 const router = require('./routes')
 
 const app = express()
 
 // Middleware
+app.use(passport.initialize())
 app.use(express.json())
-if (process.env.NODE_ENV === 'development') {
+app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+
+
+passportLocal(passport)
+passportJwt(passport)
+
+if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
-app.get('/', (req, res) => {
+app.get('/api/v1', (req, res) => {
     res.send('Hello friend')
 })
 
