3bf7d8c79e3df059e9b6f400c9ef5969205026ad
Carl Bebli <beblicarl.cb@gmail.com>
2022-08-15 12:36:05 +0000

    feature(ednpoint): get all articles endpoint [Start #57]

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,13 @@
-const users = require('./users')
+const express = require('express')
 
-module.exports = (app) => {
-    app.use('/users', users)
-}
+const users = require('./users')
+const posts = require('./posts')
+
+const app = express.Router()
+
+// module.exports = (app) => {
+app.use('/users', users)
+app.use('/feed', posts)
+// }
+
+module.exports = app
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,78 +1,17 @@
-// const express = require('express')
-// const morgan = require('morgan')
-// // const { Pool } = require('pg')
-// //
-// const db = require('../db')
+const express = require('express')
+const db = require('../db')
 
-// const router = express.Router()
+const router = express.Router()
 
-// const app = express()
+const fetchPosts = async (req, res) => {
+    try {
+        const allArticles = await db.query('SELECT * FROM posts')
+        res.json(allArticles.rows)
+    } catch (err) {
+        console.error(err.message)
+    }
+}
 
-// // Middleware
-// app.use(express.json())
-// if (process.env.NODE_ENV === 'development') {
-//     app.use(morgan('dev'))
-// }
+router.route('/').get(fetchPosts)
 
-// // GET ALL ARTICLES
-// app.get('/', (req, res) => {
-//     res.send('Hello friend')
-// })
-
-// // GET A ARTICLE
-
-// // CREATE A USER
-// app.post('/user', async (req, res) => {
-//     try {
-//         const {
-//             id,
-//             firstName,
-//             lastName,
-//             email,
-//             passwordHash,
-//             gender,
-//             jobRole,
-//             department,
-//             address,
-//         } = req.body
-//         const newUser = await db.query(
-//             'INSERT INTO users ( id, firstName, lastName, email, passwordHash, gender, jobRole , department, address) VALUES ($1 , $2 , $3, $4, $5, $6, $7, $8, $9) RETURNING *',
-//             [
-//                 id,
-//                 firstName,
-//                 lastName,
-//                 email,
-//                 passwordHash,
-//                 gender,
-//                 jobRole,
-//                 department,
-//                 address,
-//             ]
-//         )
-//         res.json(newUser)
-//     } catch (err) {
-//         console.error(err.message)
-//     }
-// })
-
-// // CREATE an ARTICLE
-// app.post('/article', async (req, res) => {
-//     try {
-//         const { id, title, image, content, published, authorId } = req.body
-//         const newArticle = await db.query(
-//             'INSERT INTO articles (id , title , image , content , published , authorId ) VALUES ($1 , $2 , $3 , $4 , $5 ,$6) RETURNING *',
-//             [id, title, image, content, published, authorId]
-//         )
-//         res.json(newArticle)
-//     } catch (err) {
-//         console.error(err.message)
-//     }
-// })
-// // UPDATE ARTICLES
-
-// // DELETE ARTICLES
-
-// // Routes
-// app.use('api/v1', router)
-
-// module.exports = app
+module.exports = router
