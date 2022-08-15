b36d8c98e5354679088a5075d1bfffb3ca0a813d
jakazzy <jakazzy@gmail.com>
2022-08-15 18:25:57 +0000

    add password jwt

---
+++ src/lib/passport-jwt.js
@@ -0,0 +1,28 @@
+/* eslint-disable camelcase */
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt')
+const db = require('../db')
+const config = require('../config')
+
+const options = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey:config('SECRET')
+}
+
+module.exports = ( passport ) => {
+    const verifyCallBack = (jwt_payload, done) => {
+        
+        db.findOne({id: jwt_payload.sub}, (err, user) =>{
+            if (err) {
+                return done(err, false);
+            }
+            if (user) {
+                return done(null, user);
+            } 
+                return done(null, false);
+                // or you could create a new account        
+        });
+    }
+    
+    // passport.use(new JwtStrategy(options, verifyCallBack));
+}
+
