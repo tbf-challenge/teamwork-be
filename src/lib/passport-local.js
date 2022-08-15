b3e24c5709f8ee9a6f6f8b88e788162e5b88a2d6
jakazzy <jakazzy@gmail.com>
2022-08-15 18:26:17 +0000

    add password local

---
+++ src/lib/passport-local.js
@@ -0,0 +1,31 @@
+/* eslint-disable camelcase */
+const LocalStrategy = require('passport-local')
+const db = require('../db')
+
+const options = {
+    usernameField : 'email',
+    passwordField : 'password',
+    passReqToCallback : true
+}
+
+module.exports = ( passport ) => {
+    const loginVerifyCallBack = (email, password, done) => {
+        db.findOne({ email },  (err, user)=> {
+          if (err) { return done(err, false) }
+          if (!user) { return done(null, false); }
+          if (!user.verifyPassword(password)) { return done(null, false); }
+          return done(null, user);
+        });
+      }
+
+    const signupVerifyCallBack = (email, password, done) => {
+        db.findOne({ email },  (err, user)=> {
+          if (err) { return done(err, false) }
+          if (!user) { return done(null, false); }
+          if (!user.verifyPassword(password)) { return done(null, false); }
+          return done(null, user);
+        });
+      }
+    passport.use('local-login', new LocalStrategy(options, loginVerifyCallBack))
+    passport.use('local-signup', new LocalStrategy(options, signupVerifyCallBack))
+}
