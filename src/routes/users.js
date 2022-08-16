25da13c38f78448acec06b3c57ec0d660ff2be49
Carl Bebli <beblicarl.cb@gmail.com>
2022-08-16 20:05:11 +0000

    reset main

--- src/routes/users.js
+++ src/routes/users.js
@@ -12,14 +12,19 @@ const getUser = async (req, res) => {
 }
 const updateUser = () => {}
 const deleteUser = () => {}
-const checkId = (req, res, next, val) => {
+const checkId = (req, res, next , val ) => {
     console.log(`user id is ${val}`)
     next()
 }
 
 router.param('id', checkId)
-router.route('/').get(fetchUsers).post(createUsers)
+router.route('/')
+.get(fetchUsers)
+.post(createUsers)
 
-router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
+router.route('/:id')
+.get(getUser)
+.patch(updateUser)
+.delete(deleteUser)
 
 module.exports = router
