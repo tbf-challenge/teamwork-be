f4efa7fb61946bc499d07f9b832de91dba38ad48
Carl Bebli <beblicarl.cb@gmail.com>
2022-08-16 17:49:51 +0000

    feature(endpoint): delete article endpoint [Start #63]

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,24 +3,23 @@ const db = require('../db')
 
 const router = express.Router()
 
-const fetchUsers = (req, res) => {
-    res.send('Hello')
-}
+const fetchUsers = () => {}
 const createUsers = () => {}
 const getUser = async (req, res) => {
     const { id } = req.params
     const { rows } = await db.query('SELECT * FROM users WHERE id = $1', [id])
     res.send(rows[0])
 }
-
 const updateUser = () => {}
 const deleteUser = () => {}
+const checkId = (req, res, next, val) => {
+    console.log(`user id is ${val}`)
+    next()
+}
 
-router.route('/').get(fetchUsers)
-router.route('/').post(createUsers)
+router.param('id', checkId)
+router.route('/').get(fetchUsers).post(createUsers)
 
-router.route('/:id').get(getUser)
-router.route('/:id').patch(updateUser)
-router.route('/:id').delete(deleteUser)
+router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
 
 module.exports = router
