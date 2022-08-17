0b35cc61838f60739a7b0bf013bdf5ab0787b6c2
Carl Bebli <beblicarl.cb@gmail.com>
2022-08-17 11:57:18 +0000

    feature(response body): Added response body [Start #45]

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -4,19 +4,30 @@ const db = require('../db')
 
 const router = express.Router()
 
+const dateTime = new Date().toJSON()
+
 // CREATE AN ARTICLE
 
-const createPosts = async (req, res) => {
+const createPosts = async (req, res, next) => {
     try {
         const { id, userId, title, image, content, published, createdAt } =
             req.body
-        const newArticle = await db.query(
+        await db.query(
             'INSERT INTO posts (id , "userId", title , image , content , published , "createdAt" ) VALUES ($1 , $2 , $3 , $4 , $5 ,$6 , $7) RETURNING *',
             [id, userId, title, image, content, published, createdAt]
         )
-        res.json(newArticle)
+        res.status(201).json({
+            status: 'success',
+            data: {
+                message: 'Article successfully posted',
+                id,
+                createdOn: dateTime,
+                title,
+            },
+        })
     } catch (err) {
         console.error(err.message)
+        next(err)
     }
 }
 
