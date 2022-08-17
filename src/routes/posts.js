4cb941338109594e2808126f21c167d2b1d8950b
Carl Bebli <beblicarl.cb@gmail.com>
2022-08-17 20:19:59 +0000

    feature(endpoint): create articles endpoint [Start #45]

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -8,22 +8,23 @@ const router = express.Router()
 
 const createPost = async (req, res, next) => {
     try {
-        const { id, userId, title, image, content, published } = req.body
+        const { userId, title, image, content, published } = req.body
         const newArticle = await db.query(
-            'INSERT INTO posts ( id , "userId", title , image , content , published ) VALUES ($1 , $2 , $3 , $4 , $5 , $6) RETURNING *',
-            [id, userId, title, image, content, published]
+            'INSERT INTO posts ("userId", title , image , content , published ) VALUES ($1 , $2 , $3 , $4 , $5 ) RETURNING *',
+            [userId, title, image, content, published]
         )
+        const articleBody = newArticle.rows[0]
         res.status(201).json({
             status: 'success',
             data: {
                 message: 'Article successfully posted',
-                id: newArticle.id,
-                userId,
-                title,
-                image,
-                content,
-                published,
-                createdAt: 'current_timestamp',
+                id: articleBody.id,
+                userId: articleBody.userId,
+                title: articleBody.title,
+                image: articleBody.image,
+                content: articleBody.content,
+                published: articleBody.published,
+                createdAt: articleBody.createdAt,
             },
         })
     } catch (err) {
