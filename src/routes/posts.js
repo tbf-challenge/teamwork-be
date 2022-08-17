42efcd1e1631b1a2362a84470be6912e32b0d14b
Carl Bebli <beblicarl.cb@gmail.com>
2022-08-17 11:09:37 +0000

    feature(endpoint) create comment endpoint [Start #66]

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -4,16 +4,28 @@ const db = require('../db')
 
 const router = express.Router()
 
+const date = new Date(Date.UTC(2012, 11, 12, 3, 0, 0))
+
 // CREATE A POST COMMENT
 
 const createComment = async (req, res) => {
     try {
         const { id, userId, postId, content, published, createdAt } = req.body
-        const newComment = await db.query(
+        await db.query(
             'INSERT INTO comments (id , "userId", "postId", content , published , "createdAt" ) VALUES ($1 , $2 , $3 , $4 , $5 ,$6 ) RETURNING *',
             [id, userId, postId, content, published, createdAt]
         )
-        res.json(newComment)
+
+        res.status(201).json({
+            status: 'success',
+            data: {
+                message: 'Comment successfully created',
+                createdOn: date.toLocaleDateString(),
+                articleTitle: String,
+                article: String,
+                comment: String,
+            },
+        })
     } catch (err) {
         console.error(err.message)
     }
