6c2465dd2ade42b37e2afc1be9e1ddf959b34885
jakazzy <jakazzy@gmail.com>
2022-08-17 05:28:46 +0000

    lint password gen file

--- src/lib/passwordlib.js
+++ src/lib/passwordlib.js
@@ -1,8 +1,10 @@
 const bcrypt   = require('bcrypt-nodejs');
 
-const genPasswordHash = password => bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
+const genPasswordHash = password => 
+bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
 
-const verifyPassword = (password, hashPassword) => bcrypt.compareSync(password, hashPassword)
+const verifyPassword = (password, hashPassword) => 
+bcrypt.compareSync(password, hashPassword)
 
 module.exports ={
     genPasswordHash,
