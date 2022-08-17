9e1b044f63cbe0128f186335c577e328636eb00e
jakazzy <jakazzy@gmail.com>
2022-08-17 04:54:13 +0000

    create logger lib

---
+++ src/lib/logger.js
@@ -0,0 +1,23 @@
+const chalk= require('chalk')
+
+const error = chalk.bold.red;
+const warn = chalk.hex('#FFA500'); // Orange color
+const info = chalk.bold.blue;
+
+
+/* eslint-disable no-console */
+const log = () => ({
+    error(message){
+        console.log(error(message))
+    },
+
+    warn(message){
+        console.log(warn(message))
+    },
+
+    info(message){
+        console.info(info(message))
+    }
+ })
+
+module.export = log
