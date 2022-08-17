4e2a6e03995e446975bebff76a3bd18c7f25cd50
jakazzy <jakazzy@gmail.com>
2022-08-17 23:19:05 +0000

    lint passport file

--- src/lib/passport-local.js
+++ src/lib/passport-local.js
@@ -3,21 +3,21 @@ const LocalStrategy = require('passport-local')
 const db = require('../db')
 
 const options = {
-    usernameField : 'email',
-    passwordField : 'password',
-    passReqToCallback : true
+	usernameField : 'email',
+	passwordField : 'password',
+	passReqToCallback : true
 }
 
 module.exports = ( passport ) => {
-    const loginVerifyCallBack = (email, password, done) => {
-        db.findOne({ email },  (err, user)=> {
-          if (err) { return done(err, false) }
-          if (!user) { return done(null, false) }
-          if (!user.verifyPassword(password)) { return done(null, false) }
-          return done(null, user)
-        })
-      }
+	const loginVerifyCallBack = (email, password, done) => {
+		db.findOne({ email },  (err, user)=> {
+			if (err) { return done(err, false) }
+			if (!user) { return done(null, false) }
+			if (!user.verifyPassword(password)) { return done(null, false) }
+			return done(null, user)
+		})
+	}
 
-    passport.use(
-      'local-login', new LocalStrategy(options, loginVerifyCallBack))
+	passport.use(
+		'local-login', new LocalStrategy(options, loginVerifyCallBack))
 }
