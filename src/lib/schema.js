4afe9780db63d74729a256b09bff3005a5a2017e
jakazzy <jakazzy@gmail.com>
2022-08-17 23:26:27 +0000

    lint passport schema file

--- src/lib/schema.js
+++ src/lib/schema.js
@@ -2,64 +2,64 @@
 const Joi = require('joi')
 
 const authSchema = Joi.object({
-    firstName: Joi.string()
-        .alphanum()
-        .min(3)
-        .max(30)
-        .required(),
+	firstName: Joi.string()
+		.alphanum()
+		.min(3)
+		.max(30)
+		.required(),
         
-    lastName: Joi.string()
-        .alphanum()
-        .min(3)
-        .max(30)
-        .required(),
+	lastName: Joi.string()
+		.alphanum()
+		.min(3)
+		.max(30)
+		.required(),
 
-    password: Joi.string()
-        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+	password: Joi.string()
+		.pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
 
-    email: Joi.string()
-        .email({ 
-            minDomainSegments: 2, 
-            tlds: { 
-                allow: ['com', 'net'] 
-            } 
-        })
-        .required(),
-        gender: Joi.string().valid(
-            'female', 
-            'male', 
-            'non-binary',
-            'transgender', 
-            'intersex', 
-            'none'
-            )
-        .lowercase()
-        .required(),
-        jobRole: Joi.string().valid(
-            'admin', 
-            'user'
-            )
-            .lowercase()
-            .required(),
-        department: Joi.string()
-        .valid(
-            'sales', 
-            'human resource', 
-            'product development', 
-            'infrastructure', 
-            'security', 
-            'research and development', 
-            'customer support', 
-            'software developers'
-        )
-        .lowercase()
-        .required(),
-        address:  Joi.string()
-        .min(3)
-        .max(100)
-        .required()
+	email: Joi.string()
+		.email({ 
+			minDomainSegments: 2, 
+			tlds: { 
+				allow: ['com', 'net'] 
+			} 
+		})
+		.required(),
+	gender: Joi.string().valid(
+		'female', 
+		'male', 
+		'non-binary',
+		'transgender', 
+		'intersex', 
+		'none'
+	)
+		.lowercase()
+		.required(),
+	jobRole: Joi.string().valid(
+		'admin', 
+		'user'
+	)
+		.lowercase()
+		.required(),
+	department: Joi.string()
+		.valid(
+			'sales', 
+			'human resource', 
+			'product development', 
+			'infrastructure', 
+			'security', 
+			'research and development', 
+			'customer support', 
+			'software developers'
+		)
+		.lowercase()
+		.required(),
+	address:  Joi.string()
+		.min(3)
+		.max(100)
+		.required()
 })
 
 module.exports = {
-    '/create-user': authSchema
-  }
+	'/create-user': authSchema
+}
