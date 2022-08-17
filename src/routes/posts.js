c6891727e571e4d89ba8f44774387328337be471
Carl Bebli <beblicarl.cb@gmail.com>
2022-08-17 13:29:59 +0000

    feature(response object): response object [Start #59]

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -6,25 +6,21 @@ const router = express.Router()
 
 // GET AN ARTICLE BY ID
 
-const getPost = async (req, res) => {
+const getPost = async (req, res, near) => {
     const { id } = req.params
     try {
         const article = await db.query('SELECT * FROM posts WHERE id = $1 ', [
             id,
         ])
-        res.json(article.rows[0])
+        res.status(201).json(article.rows[0])
     } catch (err) {
         console.error(err.message)
+        near(err)
     }
 }
 
-const checkId = (req, res, next, val) => {
-    console.log(`user id is ${val}`)
-    next()
-}
-
 // Routes
-router.param('id', checkId)
+
 router.route('/:id').get(getPost)
 
 module.exports = router
