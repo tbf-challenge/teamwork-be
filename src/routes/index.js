3e3922249911940b91f5e180b51a8119c6da712e
yellow_may <yellow.may.21@gmail.com>
2022-08-18 12:52:54 +0100

    feature(tags): get all tags endpoint [Start #70]

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 
 const users = require('./users')
 const posts = require('./posts')
+const tags = require('./tags')
 
 const app = express.Router()
 
 app.use('/users', users)
 app.use('/articles', posts)
+app.use('/tags', tags)
 
 module.exports = app
---
+++ src/routes/tags.js
@@ -0,0 +1,18 @@
+const tagRouter = require('express').Router()
+const db = require('../db')
+
+// FETCH ALL PREDEFINED TAGS
+
+const fetchTags = async (_req, res) => {
+    try {
+        const tags = await db.query('SELECT * FROM tags')
+        res.status(200).json(tags.rows)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Internal server error', error })
+    }
+}
+
+tagRouter.route('/').get(fetchTags)
+
+module.exports = tagRouter
