93e1ded275da3dbb904511933b7c3da8bd1c5a4c
jakazzy <jakazzy@gmail.com>
2022-08-18 08:30:19 +0000

    get user email to retrieve user

--- src/lib/passport-jwt.js
+++ src/lib/passport-jwt.js
@@ -1,28 +1,27 @@
 /* eslint-disable camelcase */
 const { Strategy, ExtractJwt } = require('passport-jwt')
-const db = require('../db')
+const { getUserByEmail } = require('../services/users')
+
 const config = require('../config')
 
 const options = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey:config('SECRET')
+	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+	secretOrKey:config('SECRET')
 }
 
 module.exports = ( passport ) => {
-    const verifyCallBack = (jwt_payload, done) => {
+	const verifyCallBack =  async (jwt_payload, done) => {
+		const { email } = jwt_payload.user
+		const user = await getUserByEmail(email)
         
-        db.findOne({id: jwt_payload.sub}, (err, user) =>{
-            if (err) {
-                return done(err, false)
-            }
-            if (user) {
-                return done(null, user)
-            } 
-                return done(null, false)
-                // or you could create a new account        
-        })
-    }
+		if (user) {
+			return done(null, user)
+		} 
+		return done(null, false)
+			       
+		
+	}
     
-    passport.use(new Strategy(options, verifyCallBack))
+	passport.use(new Strategy(options, verifyCallBack))
 }
 
