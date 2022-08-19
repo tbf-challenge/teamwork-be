83de5d491d5deb8c5a48377fdec86914c68e7806
yellow_may <yellow.may.21@gmail.com>
2022-08-19 11:05:20 +0100

    feature(tags):create new tag endpoint[Starts #70]

--- src/routes/tags.js
+++ src/routes/tags.js
@@ -13,8 +13,31 @@ const fetchTags = async (_req, res) => {
     }
 }
 
-// ASSIGN TAG TO POST
+// CREATE NEW TAG
 
-tagRouter.route('/').get(fetchTags)
+const createTag = async (req, res) => {
+    try {
+        const { content, title } = req.body
+
+        const newTag = await db.query(
+            'INSERT INTO tags (content, title) SELECT $1, $2 WHERE NOT EXISTS (SELECT * FROM tags WHERE title = $2) RETURNING *',
+            [content, title]
+        )
+
+        if (!newTag.rows[0]) {
+            res.status(400).json({ message: 'Tag already exits' })
+        } else {
+            res.status(202).json({
+                message: 'Successfully created a new tag',
+                data: newTag.rows[0],
+            })
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Internal server error', error })
+    }
+}
+
+tagRouter.route('/').get(fetchTags).post(createTag)
 
 module.exports = tagRouter
