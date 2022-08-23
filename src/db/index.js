1ba256e27e68d1f0645795c647fa2a28b876f794
jakazzy <jakazzy@gmail.com>
2022-08-23 07:49:26 +0000

    enhancement(x): fix error with the db url

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,12 @@
-/* eslint-disable no-console */
 const { Pool } = require('pg')
+const { logger }= require('../lib')
+
+const log = logger()
 
 const config = require('../config')
 
 const pool = new Pool({ connectionString: config('DATABASE_URL')})
-console.log('database successfully connected')
+log.success('database successfully connected')
 module.exports = {
 	async query (text, params) {
 		const res = await pool.query(text, params)
