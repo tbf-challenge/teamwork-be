a7863990e10bd0c39c7406737104cb901ea61c61
jakazzy <jakazzy@gmail.com>
2022-09-01 20:06:03 +0000

    feat(x): create new users

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,26 @@
 const express = require("express")
+const { catchAsync } = require('../lib')
+
 const db = require("../db")
 const isAuthenticated = require("../middleware/isAuthenticated")
+const userService = require('../services/users')
 
 const router = express.Router()
 
-const fetchUsers = (req, res) => {
-	res.send("get users")
-}
-const createUsers = () => {}
+const fetchUsers = catchAsync(async ( req, res ) => {
+
+	const users = await userService.fetchUsers()
+
+	return res.status(200).json({
+		status: 'success',
+		data: users.map(user => ({
+			id: user.id,
+			firstName: user.firstName,
+			lastName: user.lastName
+		}))
+	})
+})
+
 const getUser = async (req, res) => {
 	const { id } = req.params
 	const { rows } = await db.query("SELECT * FROM users WHERE id = $1", [id])
@@ -22,7 +35,6 @@ router.use(isAuthenticated())
 router
 	.route("/")
 	.get(fetchUsers)
-	.post(createUsers)
 router
 	.route("/:id")
 	.get(getUser)
--- src/services/users.js
+++ src/services/users.js
@@ -1,84 +1,11 @@
-const jwt = require("jsonwebtoken")
-const { genPasswordHash, verifyPassword } = require("../lib")
+const db = require('../db')
 
-const config = require("../config")
-const db = require("../db")
-const { AppError } = require("../lib")
+const fetchUsers = async() => {
 
-const invalidEmailAndPassword = "Invalid email or password."
-const createNewUser = async (user) => {
-	const [
-		firstName,
-		lastName,
-		email,
-		password,
-		gender,
-		jobRole,
-		department,
-		address
-	] = user
-	const passwordHash = await genPasswordHash(password)
-	// eslint-disable-next-line max-len
-	const { rows, error } = await db.query(
-		// eslint-disable-next-line max-len
-		'INSERT INTO users ("firstName", "lastName", "email", "passwordHash", "gender", "jobRole", "department", "address") VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *',
-		[
-			firstName,
-			lastName,
-			email,
-			passwordHash,
-			gender,
-			jobRole,
-			department,
-			address
-		]
-	)
-
-	if (error) {
-		throw error
-	}
-	const userProfile = rows[0]
-	const body = { id: userProfile.id, email: userProfile.email }
-	const token = jwt.sign({ user: body }, config("TOKEN_SECRET"))
-
-	return { token, userId: userProfile.id }
-}
-
-const getUserByEmail = async (email) => {
-	const { rows, error } = await db.query(
-		"SELECT * FROM users WHERE email = $1",
-		[email]
-	)
-
-	if (error) {
-		throw error
-	}
-	const userProfile = rows[0]
-
-	if (!userProfile) {
-		throw new AppError(invalidEmailAndPassword, 401)
-	}
-
-	const user = userProfile
-	return user
-}
-
-const signInUserByEmail = async (email, password) => {
-	const user = await getUserByEmail(email)
-
-	const isPasswordSame = await verifyPassword(password, user.passwordHash)
-	if (!isPasswordSame) {
-		throw new AppError(invalidEmailAndPassword, 401)
-	}
-
-	const body = { id: user.id, email: user.email }
-	const token = jwt.sign({ user: body }, config("TOKEN_SECRET"))
-
-	return { token, userId: user.id }
+	const users = await db.query('SELECT * FROM users')
+	return users.rows
 }
 
 module.exports = {
-	createNewUser,
-	getUserByEmail,
-	signInUserByEmail
+	fetchUsers
 }
