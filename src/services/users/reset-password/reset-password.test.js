15d4916729494d09695aeb3fae0dc0f11ffc1506
Gideon Balogun <balogungideon12@gmail.com>
2022-09-22 16:07:33 +0100

    add reset-password service

---
+++ src/services/users/reset-password/reset-password.test.js
@@ -0,0 +1,25 @@
+const chai = require('chai')
+const verifyResetToken = require('./verifyPasswordResetToken')
+const generateAccessToken = require('../generate-access-token')
+
+
+const testEmail = 'testemail@gmail.com'
+
+        
+const { expect } = chai
+
+describe('verifyPasswordResetToken', () => {
+	it('should throw an error if the token is invalid', () => {
+		const token = 'invalid-token'
+		expect(() => verifyResetToken(token)).to.throw()
+	})
+})
+describe('verifyPasswordResetToken', () => { 
+	it('should return the decoded token if the token is valid', () => {
+		const token = generateAccessToken(
+			{ data: { email: testEmail }, 
+				expiry: '1h' })
+		const decodedEmail = 'testemail@gmail.com'
+		expect(verifyResetToken(token).email).to.deep.equal(decodedEmail)
+	})
+})
