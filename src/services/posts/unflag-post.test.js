ab78f8b131d794853d1de85bf655dcac4eda2246
Gideon Balogun <balogungideon12@gmail.com>
2022-10-06 15:45:09 +0100

    add test for unflagging an article

--- src/services/posts/unflag-post.test.js
+++ src/services/posts/unflag-post.test.js
@@ -37,5 +37,33 @@ describe('UNFLAG a post', () => {
 	   })
    
 	})
+
+	describe('ARTICLE', () => {
+		let post
+		before(async ()=>{
+			post = await fixtures.insertPost({
+				userId : user.id , 
+				type : 'article'
+			})
+		})
+		it('should delete an article flag in the database', async () => {
+			await fixtures.insertPost({
+			   userId : user.id , 
+			   type : 'article'
+		   })
+			
+			await fixtures.insertPostFlag({
+				userId : user.id ,
+				postId : post.id
+			})
+		   await unflagPost({userId : user.id , postId : post.id})
+		   const result = await db.query(
+			   `SELECT * FROM post_flags
+				WHERE "userId" = $1
+				AND "postId" = $2`,[ user.id, post.id ])
+		   expect(result.rowCount).to.eql(0)	 
+	   })
+   
+	})
 	
 })
