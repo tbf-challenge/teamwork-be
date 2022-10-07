db4e9a670eb9336955a454e415be608fa0b11eb5
Carl Bebli <beblicarl.cb@gmail.com>
2022-10-07 16:31:50 +0000

    feature(x): refactored the function for seeding the db [Start #153]

--- migrations/1665144863994_seed-db-with-predefined-tags.js
+++ migrations/1665144863994_seed-db-with-predefined-tags.js
@@ -4,34 +4,9 @@ exports.shorthands = undefined
 exports.up = (pgm) => {
 	pgm.sql(`INSERT into tags
             (title)
-            VALUES ('Life')`)
-	pgm.sql(`INSERT into tags
-            (title)
-            VALUES ('Startup')`)
-	pgm.sql(`INSERT into tags
-            (title)
-            VALUES ('Blockchain')`)
-	pgm.sql(`INSERT into tags
-            (title)
-            VALUES ('Poetry')`)
-	pgm.sql(`INSERT into tags
-            (title)
-            VALUES ('Life Lessons')`)
-	pgm.sql(`INSERT into tags
-            (title)
-            VALUES ('Politics')`)
-	pgm.sql(`INSERT into tags
-            (title)
-            VALUES ('Health')`)
-	pgm.sql(`INSERT into tags
-            (title)
-            VALUES ('Love')`)
-	pgm.sql(`INSERT into tags
-            (title)
-            VALUES ('Travel')`)
-	pgm.sql(`INSERT into tags
-            (title)
-            VALUES ('Technology')`)
+            VALUES ('Life'), ('Startup'),('Blockchain'),('Poetry'),
+            ('Life Lessons'),('Politics'),('Health'),('Love'),
+            ('Travel'),('Technology')`)
 }
 
 exports.down = false
