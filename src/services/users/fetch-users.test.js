5c36aada30e4617ae1f5d275dd111d256560511e
Carl Bebli <beblicarl.cb@gmail.com>
2022-10-18 07:29:18 +0000

    implemented suggested changes for fetch users services testing

--- src/services/users/fetch-users.test.js
+++ src/services/users/fetch-users.test.js
@@ -1,27 +1,21 @@
 const { expect } = require('chai')
-const {fixtures , setupDB} = require('../../../test/utils')
+const {fixtures, setupDB } = require('../../../test/utils')
+const db = require('../../db')
 const fetchUsers = require('./fetch-users')
 
 describe('Fetch all users', () => {
-	const numberOfUsers = 5
-	let users
 	beforeEach(async () => {
 		await setupDB()
-		users = await fixtures.insertMultipleUsers(numberOfUsers)
+		await fixtures.insertUser()
+		await fixtures.insertUser()
 	 })
 
 	describe('Success', () => {
-
-		it('should return the right response', async () =>{
+		it('should fetch users data', async () =>{
 			const actualUsers = await fetchUsers()
-			return expect(actualUsers.length).to.eql(users.rowCount)	
-		})
-	})
-	describe('Failure', () => {
-
-		it('should return the wrong number of users', async () =>{
-			await fetchUsers()
-			return expect(users.rowCount).to.not.eql(6)	
+			const result = await db.query(`
+			SELECT * FROM users`)
+			return expect(actualUsers).to.eql(result.rows)	
 		})
 	})
 })
