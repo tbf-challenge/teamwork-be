28abd6beb669c34c78a155c74861cbf22ad938be
Carl Bebli <beblicarl.cb@gmail.com>
2022-10-18 08:38:31 +0000

    changed the beforeEach to before and removed the fetchUser services from the api test

--- src/routes/users/fetch-users.e2e.test.js
+++ src/routes/users/fetch-users.e2e.test.js
@@ -1,42 +1,11 @@
 const { expect } = require('chai')
 const { fixtures, setupDB } = require('../../../test/utils')
-const fetchUsers = require('../../services/users/fetch-users')
 
-describe('GET /users', () => {    
-	describe('Success', () => {
-		let adminUser
-		let accessToken
-		beforeEach(async () =>{
-			await setupDB()
-			await fixtures.insertMultipleUsers()
-			adminUser = await fixtures.insertUser({
-				role : 'admin'
-			}) 
-			const body = { id: adminUser.id, email: adminUser.email }
-			accessToken = fixtures.generateAccessToken(
-				{user : body}
-			)
-		})
-		it('should return 200 if users are fetched', async () => 
-			fixtures.api()
-				.get(`/api/v1/users`)
-				.set('Authorization', `Bearer ${accessToken}`)
-				.expect(200)
-		)
-		it('should return the right number of users', async() =>{
-			const actualUsers = await fetchUsers()
-			fixtures.api()
-				.get(`/api/v1/users`)
-				.set('Authorization', `Bearer ${accessToken}`)
-				.then(res => {
-				 expect(res.body.length).eql(actualUsers.length)
-				}) 
-		})	
-	})
+describe('GET /users', () => {   
 	describe('Failure', ()=> {
 		let userUser
 		let accessToken
-		beforeEach(async () =>{
+		before(async () =>{
 			userUser = await fixtures.insertUser({
 				role : 'user'
 			}) 
@@ -55,5 +24,34 @@ describe('GET /users', () => {
 				.set('Authorization', `Bearer ${accessToken}`)
 				.expect(403 , expectedError)
 		})
+	}) 
+	describe('Success', () => {
+		let adminUser
+		let accessToken
+		before(async () =>{
+			await setupDB()
+			await fixtures.insertMultipleUsers()
+			adminUser = await fixtures.insertUser({
+				role : 'admin'
+			}) 
+			const body = { id: adminUser.id, email: adminUser.email }
+			accessToken = fixtures.generateAccessToken(
+				{user : body}
+			)
+		})
+		it('should return 200 if users are fetched', async () => 
+			fixtures.api()
+				.get(`/api/v1/users`)
+				.set('Authorization', `Bearer ${accessToken}`)
+				.expect(200)
+		)
+		it('should return the right number of users', async() =>{
+			fixtures.api()
+				.get(`/api/v1/users`)
+				.set('Authorization', `Bearer ${accessToken}`)
+				.then(res => {
+				 expect(res.body.length).eql(6)
+				}) 
+		})	
 	})
 })
