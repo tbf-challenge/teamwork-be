5719caebe26981b708fe18a6d68fd272c414e974
Carl Bebli <beblicarl.cb@gmail.com>
2022-10-21 15:37:43 +0000

    made suggested changes

--- src/services/users/signin-user-by-email.test.js
+++ src/services/users/signin-user-by-email.test.js
@@ -6,45 +6,29 @@ const signInUserByEmail = require('./signin-user-by-email')
 describe('User signin', () => {
 	let user
 	let validPassword
-	beforeEach(async () => {
+	before(async () => {
 		user = await fixtures.insertUser({
 			password : 'validPaswword123$'
 		})
 		validPassword = 'validPaswword123$'
 	})
 
-	it('should throw an error if user does not exist', async () => {
-		const { id,email } = user
-		await db.query(
-			`DELETE FROM users
-             WHERE id = $1`, [id])
-
-		return expect(signInUserByEmail(email , validPassword))
+	it('should throw an error if user does not exist', async () => 
+		expect(signInUserByEmail('invalid-email' , validPassword))
 			.to.be.rejectedWith(
 				'Invalid email or password.')
-	})
+	)
 
 	it('should return the right response', async () => {
 		const { id, email} = user
-		await signInUserByEmail(
+		const result = await signInUserByEmail(
 			email, validPassword)
 
 		const { rows } = await db.query(
 			`SELECT * FROM users
              WHERE id = $1`, [id])
-
 		const  loggedInUser = rows[0]
-
-		return expect(loggedInUser).to.include({
-			firstName: user.firstName,
-			lastName: user.lastName,
-			email : user.email,
-			gender: user.gender,
-			jobRole: user.jobRole,
-			department: user.department,
-			address: user.address,
-			profilePictureUrl: user.profilePictureUrl
-		})
+		return expect(loggedInUser).to.eql(result.user)
 
 	})
 
