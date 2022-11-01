5fa34a4476849db60eb3b20b1c484e4552b1b2e2
Carl Bebli <beblicarl.cb@gmail.com>
2022-11-01 19:16:28 +0000

    implemented suggested changes

--- src/services/users/get-invited-user-detail.test.js
+++ src/services/users/get-invited-user-detail.test.js
@@ -6,44 +6,42 @@ const getInvitedUserDetail = require('./get-invited-user-detail')
 
 describe('Get invited user information', () => {	
 
-	describe('Failure', async()=>{
-		const	invalidInviteToken = fixtures.generateAccessToken({
-			email : faker.internet.email()
+	let inviteUserData
+	let inviteToken
+	let invalidInviteToken
+	let invite
+
+	before(async () => {
+		inviteUserData = {
+			email : faker.internet.email(),
+			status : 'pending'
+		}
+		inviteToken = fixtures.generateAccessToken(inviteUserData)
+		invite = await fixtures.insertUserInvite(
+			{email: inviteUserData.email,
+				status : 'pending'})
+		invalidInviteToken = fixtures.generateAccessToken({
+			email : faker.internet.email(),
+			status : 'pending'
+
 		})
-		it('should throw an error if the invite is invalid', async () =>
-			expect(getInvitedUserDetail(invalidInviteToken))
-				.to.be.rejectedWith(
-					'Invite is invalid/expired')
-		)
 	})
 
-	describe('Success', async()=>{
-
-		let inviteUserData
-		let inviteToken
-		let invite
-
-		before(async () => {
-			inviteUserData = {
-				email : faker.internet.email(),
-				status : 'pending'
-			}
-			inviteToken = fixtures.generateAccessToken(inviteUserData)
-			invite = await fixtures.insertUserInvite(
-				{email: inviteUserData.email,
-					status : 'pending'})
-			
-
-		})
-		it('should get invited user information', async () =>{
 	
-			const invitedUser = await getInvitedUserDetail(inviteToken)
-			const accessToken = fixtures.generateAccessToken(invite)
-			return expect(invitedUser).to.eql({
-				email : invite.email,
-				accessToken
-			})
+	it('should throw an error if the invite is invalid', async () =>
+		expect(getInvitedUserDetail(invalidInviteToken))
+			.to.be.rejectedWith(
+				'Invite is invalid/expired')
+	)
 
-		})	
-	})
+	it('should get invited user information', async () =>{
+	
+		const invitedUser = await getInvitedUserDetail(inviteToken)
+		const accessToken = fixtures.generateAccessToken(invite)
+		return expect(invitedUser).to.eql({
+			email : invite.email,
+			accessToken
+		})
+
+	})	
 })
