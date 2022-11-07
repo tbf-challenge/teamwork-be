48f59534be6002dc1ef3a6a36185be0c6c262e31
Carl Bebli <beblicarl.cb@gmail.com>
2022-11-07 11:18:08 +0000

    added resetDBTable to reset the tag table

--- src/services/tags/delete-tag.test.js
+++ src/services/tags/delete-tag.test.js
@@ -1,18 +1,24 @@
 const {expect} = require('chai')
 const db = require("../../db")
 const deleteTag = require("./delete-tag")
-const {fixtures} = require('../../../test/utils')
+const {fixtures, resetDBTable} = require('../../../test/utils')
 
 describe('DELETE tag', () => {
+
 	let tag
+
 	before(async ()=>{
+		await resetDBTable('tags')
 		tag = await fixtures.insertTag()  
 	})
+
 	it('should delete a tag', async () => {
+		
 		await deleteTag(tag.id)
 		const queryTag = await db.query(
 			`SELECT * FROM tags
                 WHERE id = $1`,[tag.id])
+	
 		expect(queryTag.rowCount).to.equal(0)
     
 	})
