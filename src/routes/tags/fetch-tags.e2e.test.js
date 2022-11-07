42f7ecb5c2cbf5b87219514b057fdde4baf1813c
Carl Bebli <beblicarl.cb@gmail.com>
2022-11-07 08:44:37 +0000

    implemented suggested changes

--- src/routes/tags/fetch-tags.e2e.test.js
+++ src/routes/tags/fetch-tags.e2e.test.js
@@ -1,12 +1,16 @@
 const { expect } = require('chai')
 const { fixtures, resetDBTable } = require('../../../test/utils')
+const { logger }  = require('../../lib')
+
+const log = logger()
+
 
 describe('GET /tags', () => {   
 
 	let user
 	let accessToken
 
-	beforeEach(async ()=> {
+	before(async ()=> {
 
 		user = await fixtures.insertUser()
 		const body = { id: user.id, email: user.email }
@@ -16,6 +20,7 @@ describe('GET /tags', () => {
 	})
 
 	describe('Failure', ()=> {
+
 		it('should return 401 if request is not authenticated', async () => 
 			fixtures.api()
 				.get('/api/v1/tags/')
@@ -37,14 +42,17 @@ describe('GET /tags', () => {
 			
 		})
 
-		it('should return 200 if tags are fetched', async () =>
-			fixtures.api()
+		it('should return 200 if tags are fetched', async () =>{
+			log.info('should return 200 if tags are fetched ')
+			return fixtures.api()
 				.get(`/api/v1/tags`)
 				.set('Authorization', `Bearer ${accessToken}`)
-				.expect(200))
+				.expect(200)
+		})
 
-		it('should return the right number of tags', async() =>
-			fixtures.api()
+		it('should return the right number of tags', async() =>{
+			log.info('should return the right number of tags ')
+			return fixtures.api()
 				.get(`/api/v1/tags`)
 				.set('Authorization', `Bearer ${accessToken}`)
 				.then(res => {
@@ -56,6 +64,6 @@ describe('GET /tags', () => {
 
 						})))
 				}) 
-		)	
+		})	
 	})
 })
