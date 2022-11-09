1e80a1fceef2d2d4671f284e40b755d76532ec81
Carl Bebli <beblicarl.cb@gmail.com>
2022-11-09 18:11:19 +0000

    returned specific fields in the getPost query

--- src/services/posts/get-post.js
+++ src/services/posts/get-post.js
@@ -9,11 +9,13 @@ const customError = require("../../lib/custom-error")
 
 const getPost = async({id, type}) => {
 	const result = await db.query(
-		`SELECT p.*, jsonb_agg(c.* ORDER BY c."createdAt" DESC) as comments
-	FROM posts p 
-	LEFT JOIN comments c ON p.id = c."postId"
-	WHERE p.id=$1 AND p.type =$2
-	GROUP BY p.id;`,
+		`SELECT p.id, p."userId", p.title, p.image, p.content, p.published,
+		p."createdAt", p.type, 
+		jsonb_agg(c.* ORDER BY c."createdAt" DESC) as comments
+		FROM posts p 
+		LEFT JOIN comments c ON p.id = c."postId"
+		WHERE p.id=$1 AND p.type =$2
+		GROUP BY p.id;`,
 		[id, type]
 	)
 	const post = result.rows[0]
