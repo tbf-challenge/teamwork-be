96e7d6447ebb1b5eb30c00e87c6c346008bb2620
Carl Bebli <beblicarl.cb@gmail.com>
2022-11-10 10:35:36 +0000

    reset delete tag test

--- src/services/tags/delete-tag.test.js
+++ src/services/tags/delete-tag.test.js
@@ -4,21 +4,15 @@ const deleteTag = require("./delete-tag")
 const {fixtures} = require('../../../test/utils')
 
 describe('DELETE tag', () => {
-
 	let tag
-
 	before(async ()=>{
-		
 		tag = await fixtures.insertTag()  
 	})
-
 	it('should delete a tag', async () => {
-		
 		await deleteTag(tag.id)
 		const queryTag = await db.query(
 			`SELECT * FROM tags
                 WHERE id = $1`,[tag.id])
-	
 		expect(queryTag.rowCount).to.equal(0)
     
 	})
