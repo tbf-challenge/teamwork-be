34c9a4c775d24bd821538f878a07ee7d48fc97d3
Carl Bebli <beblicarl.cb@gmail.com>
2022-11-13 18:27:09 +0000

    aligned the code in the migration properly to make it readable

--- migrations/1668360468072_add-trigger-function-to-likesCount-on-posts.js
+++ migrations/1668360468072_add-trigger-function-to-likesCount-on-posts.js
@@ -8,30 +8,24 @@ exports.up = (pgm) => {
 		language : "plpgsql"
 	},
 	`DECLARE postLike post_likes%ROWTYPE;
-
     BEGIN
-    
-     IF (TG_OP = 'DELETE') THEN
-     postLike = OLD;
-    
-    
-     ELSIF (TG_OP = 'INSERT') THEN
-     postLike = NEW;
-    END IF;
-    
-    UPDATE posts
-    SET "likesCount" = (
-        SELECT COUNT(*) 
-        FROM "post_likes"
-        WHERE "postId" = postLike."postId"
-    )
-    WHERE id = postLike."postId";
-    
-    RETURN postLike;
-    
-    
-    
-    END;`
+     
+       IF (TG_OP = 'DELETE') THEN
+         postLike = OLD;
+       ELSIF (TG_OP = 'INSERT') THEN
+         postLike = NEW;
+     END IF;
+     UPDATE posts
+     SET "likesCount" = (
+         SELECT COUNT(*) 
+         FROM "post_likes"
+         WHERE "postId" = postLike."postId"
+     )
+     WHERE id = postLike."postId";
+     
+     RETURN postLike;
+     
+     END;`
 	)
 
 	pgm.createTrigger("post_likes","likesTrigger",{
