fa8bca7741866e53b4927c15f326d83726430724
Carl Bebli <beblicarl.cb@gmail.com>
2022-11-14 09:28:22 +0000

    added a test case to compare number of likes in the post_likes table to likesCount on post

--- src/services/posts/delete-post.test.js
+++ src/services/posts/delete-post.test.js
@@ -5,10 +5,13 @@ const {fixtures} = require('../../../test/utils')
 
 
 describe('DELETE post', () => {
+
 	let user
+
 	before(async ()=>{
 		 user = await fixtures.insertUser()  
 	})
+
 	it('should delete a gif', async () => {
 		
 	    const post = await fixtures.insertPost({
@@ -22,6 +25,7 @@ describe('DELETE post', () => {
 		expect(queryPost.rowCount).to.equal(0)
 
 	})
+
 	it('should delete an article', async () => {
 		
 	    const post = await fixtures.insertPost({
@@ -34,6 +38,30 @@ describe('DELETE post', () => {
              WHERE id = $1`,[post.id])
 		expect(queryPost.rowCount).to.equal(0)
 
+	})
+
+	it('should decrement likesCount on post', async () => {
+		const newPost = await fixtures.insertPost({
+			userId : user.id , 
+			type : 'article'
+		})
+		await deletePost({ 
+			userId: user.id, 
+			postId: newPost.id
+	 })
+		const result = await db.query(
+			`SELECT "likesCount" FROM posts
+		 WHERE id = $1`,[ newPost.id ])
+
+		const {likesCount} = result.rows[0]
+
+		const query = await db.query(
+			`SELECT COUNT(*) FROM post_likes
+		 WHERE "postId" = $1`,[ newPost.id ])
+		
+		const unlikedPost = Number(query.rows[0].count)
+		
+		expect(unlikedPost).to.eql(likesCount)	 
 	})
 
 })
