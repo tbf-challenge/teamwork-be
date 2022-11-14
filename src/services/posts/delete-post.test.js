4e12370536db4b9063fdc6a16bc76aeae7f0184c
Carl Bebli <beblicarl.cb@gmail.com>
2022-11-14 12:00:27 +0000

    removed an it block from the deletePost test because it doesn't belong there

--- src/services/posts/delete-post.test.js
+++ src/services/posts/delete-post.test.js
@@ -5,13 +5,10 @@ const {fixtures} = require('../../../test/utils')
 
 
 describe('DELETE post', () => {
-
 	let user
-
 	before(async ()=>{
 		 user = await fixtures.insertUser()  
 	})
-
 	it('should delete a gif', async () => {
 		
 	    const post = await fixtures.insertPost({
@@ -25,7 +22,6 @@ describe('DELETE post', () => {
 		expect(queryPost.rowCount).to.equal(0)
 
 	})
-
 	it('should delete an article', async () => {
 		
 	    const post = await fixtures.insertPost({
@@ -38,31 +34,6 @@ describe('DELETE post', () => {
              WHERE id = $1`,[post.id])
 		expect(queryPost.rowCount).to.equal(0)
 
-	})
-
-	it('should decrement likesCount on post', async () => {
-		const newPost = await fixtures.insertPost({
-			userId : user.id , 
-			type : 'article'
-		})
-		await deletePost({ 
-			userId: user.id, 
-			postId: newPost.id
-	 })
-		const result = await db.query(
-			`SELECT "likesCount" FROM posts
-		 WHERE id = $1`,[ newPost.id ])
-
-		const {likesCount} = result.rows[0]
-
-		const postLikesCountQueryResult = await db.query(
-			`SELECT COUNT(*) FROM post_likes
-		 WHERE "postId" = $1`,[ newPost.id ])
-		
-		const postLikeCount = Number(
-			postLikesCountQueryResult.rows[0].count)
-		
-		expect(postLikeCount).to.eql(likesCount)
 	})
 
 })
