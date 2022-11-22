d2a963e870e3dd89d1431b7e45fb725478d7d2b7
Carl Bebli <beblicarl.cb@gmail.com>
2022-11-22 16:16:50 +0000

    improved the fetchPosts function to include request query

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,34 +2,29 @@ const express = require('express')
 const isAuthenticated = require('../middleware/isAuthenticated')
 const { catchAsync} = require('../lib')
 const postService = require('../services/posts')
+const validateSchema = require('../middleware/validateSchema')
 const {
 	transformArticleResponse ,
 	transformGifResponse
 } = require('./common/transformers')
+const { fetchPostsSchema } = require('../schema')
+const isFlaggedAndIsAdmin = require('../middleware/isFlaggedAndIsAdmin')
 
 const typeTransformMap = {
 	article : transformArticleResponse,
 	gif : transformGifResponse
 }
 
-const router = express.Router()
 
+const router = express.Router()
 
 // GET REQUESTS
 
 // GET ALL ARTICLES
 
 const fetchPosts = catchAsync( async(req, res) => {
-	let feed
 	const {isFlagged} = req.query
-
-	
-	if (isFlagged === 'true' && req.user.role === 'admin'){
-		feed = await postService.fetchFlaggedPosts()
-	}
-	 else {
-		feed = await postService.fetchPosts()
-	 }
+	const feed = await postService.fetchPosts(isFlagged)
 
 	res.status(200).json({
 		status: 'success',
@@ -54,7 +49,6 @@ router.use(isAuthenticated())
 
 router
 	.route('/')
-	.get(fetchPosts)
-// router.route('/query')
-// 	.get(isAdmin, fetchFlaggedPosts)
+	.get( validateSchema(fetchPostsSchema), isFlaggedAndIsAdmin, fetchPosts)
+
 module.exports = router
