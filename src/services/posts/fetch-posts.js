9d2e3d1aff4eab5a8866c789df46af882c214b6e
Carl Bebli <beblicarl.cb@gmail.com>
2022-11-24 10:14:34 +0000

    formed a query based on the factors so  we'll not have to repeat the common parts of the same query when we get more query parameters

--- src/services/posts/fetch-posts.js
+++ src/services/posts/fetch-posts.js
@@ -1,43 +1,35 @@
 const db = require("../../db")
 
 const fetchPosts = async(isFlagged) => {
+
 	
-	let feed 
-	if(isFlagged){
-		feed = await db.query(`
-		SELECT posts.id, posts."userId" , posts.title, posts.image,
-		posts.content,posts.published, posts."createdAt", posts.type,
-		posts."likesCount", posts."flagsCount",
-		users."firstName", users."lastName", users.email,
-		users."profilePictureUrl" 
-		FROM posts
-		INNER JOIN users on posts."userId" = users.id
-		WHERE posts."flagsCount" > 0
-		ORDER BY "flagsCount" DESC;
-		`)
-	}
-	else if(isFlagged === false){
-		feed = await db.query(`
-		SELECT posts.id, posts."userId" , posts.title, posts.image,
-		posts.content,posts.published, posts."createdAt", posts.type,
-		posts."likesCount", posts."flagsCount",
-		users."firstName", users."lastName", users.email,
-		users."profilePictureUrl" 
-		FROM posts
-		INNER JOIN users on posts."userId" = users.id
-		WHERE posts."flagsCount" = 0
-		ORDER BY "flagsCount" DESC;
-		`)
-	}
-	else(
-		feed = await db.query(`
-	SELECT posts.id, posts."userId" , posts.title, posts.image, posts.content,
-	posts.published, posts."createdAt", posts.type, posts."likesCount",
-	users."firstName", users."lastName", users.email, users."profilePictureUrl" 
-	FROM posts
-	INNER JOIN users on posts."userId" = users.id;
-	`)
-	)
+	let selectClause = `
+	SELECT posts.id, posts."userId" , posts.title, posts.image,
+	posts.content,posts.published, posts."createdAt", posts.type,
+	posts."likesCount",
+	users."firstName", users."lastName", users.email,
+	users."profilePictureUrl" 
+	`
+
+	 const fromClause = `FROM posts`
+	  
+	 const joinClause = `INNER JOIN users on posts."userId" = users.id`
+	 let whereClause = ''
+	 let orderByClause = ''
+	   
+	 if(isFlagged !== undefined){
+		 selectClause+= `, posts."flagsCount"`
+		 
+		whereClause += `
+			WHERE posts."flagsCount" ${isFlagged ? '>': '='} 0`
+		orderByClause+=`ORDER BY "flagsCount" DESC`
+	 }
+
+	const feed = await db.query(`
+${selectClause} ${fromClause} ${joinClause} ${whereClause} ${orderByClause};
+`
+	) 
+	
 	return feed.rows
 
 }
