ba1352bbefd211d9ea5fa89170e66c2f8d4c33f8
Carl Bebli <beblicarl.cb@gmail.com>
2023-01-27 17:40:41 +0000

    removed the console log, changed the limitBy to limitClause and made the cursor dynamic

--- src/services/posts/fetch-posts.js
+++ src/services/posts/fetch-posts.js
@@ -1,8 +1,7 @@
 const db = require("../../db")
 
 const fetchPosts = async(isFlagged, cursor, limit) => {
-
-	console.log(limit)	
+	
 	let selectClause = `
 	SELECT posts.id, posts."userId" , posts.title, posts.image,
 	posts.content,posts.published, posts."createdAt", posts.type,
@@ -14,21 +13,26 @@ const fetchPosts = async(isFlagged, cursor, limit) => {
 	 const fromClause = `FROM posts`
 	  
 	 const joinClause = `INNER JOIN users on posts."userId" = users.id`
-	 let whereClause = `WHERE posts."id" > ${cursor}` 
+	 let whereClause = ''
 	 let orderByClause = `ORDER BY posts."createdAt" DESC`
-	 const limitBy = `LIMIT ${limit}`
-	   
+	 const limitClause = `LIMIT ${limit}`
+
+	 // eslint-disable-next-line no-unused-expressions
+	 cursor !== undefined ? 
+	 whereClause = `WHERE posts."id" > ${cursor}` :
+	 whereClause = '' 
+
 	 if(isFlagged !== undefined){
 		 selectClause+= `, posts."flagsCount"`
 		 
-		whereClause = ` ${whereClause} 
+		whereClause += ` 
 		AND posts."flagsCount" ${isFlagged ? '>': '='} 0`
 		orderByClause =`ORDER BY "flagsCount" DESC`
 	 }
 
 	const feed = await db.query(`
 ${selectClause} ${fromClause} ${joinClause} 
-${whereClause} ${orderByClause} ${limitBy};
+${whereClause} ${orderByClause} ${limitClause};
 `
 	) 
 	
