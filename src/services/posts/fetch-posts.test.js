f3c36722bb401db3618fbd878f1da9ae59fefc1c
Carl Bebli <beblicarl.cb@gmail.com>
2023-01-30 11:47:39 +0000

    added 3 new test cases : if the function returning the correct number of posts when a limit is passed, if the function returning the correct data when a cursor is passed and if the correct response (feed and count) being returned from the function

--- src/services/posts/fetch-posts.test.js
+++ src/services/posts/fetch-posts.test.js
@@ -31,6 +31,50 @@ describe('Fetch all posts', () => {
 	    INNER JOIN users on posts."userId" = users.id
 		ORDER BY posts."createdAt" DESC LIMIT ${20};
         `)
+        
+		const value = await db.query(`SELECT COUNT(posts) FROM posts;`)
+		const countData = value.rows[0]
+		
+		const {feed, count} = actualPosts
+		return expect(feed, count).to.eql(result.rows, Number(countData.count))	
+
+	})
+
+	it('should fetch correct number of posts when limit is passed', async () =>{
+		
+		const actualPosts = await fetchPosts({limit : 3})
+
+		const result = await db.query(`
+		SELECT posts.id, posts."userId" , posts.title, posts.image,
+        posts.content,posts.published, posts."createdAt", posts.type,
+        posts."likesCount",
+        users."firstName", users."lastName", users.email, 
+        users."profilePictureUrl" 
+	    FROM posts
+	    INNER JOIN users on posts."userId" = users.id
+		ORDER BY posts."createdAt" DESC LIMIT ${3};
+        `)
+		
+		const {feed} = actualPosts
+		return expect(feed).to.eql(result.rows)	
+
+	})
+
+	it('should fetch the correct posts when cursor is passed', async () =>{
+		const cursorValue = 2
+		const actualPosts = await fetchPosts({cursor : cursorValue})
+        
+		const result = await db.query(`
+		SELECT posts.id, posts."userId" , posts.title, posts.image,
+        posts.content,posts.published, posts."createdAt", posts.type,
+        posts."likesCount",
+        users."firstName", users."lastName", users.email,
+        users."profilePictureUrl"
+         FROM posts INNER JOIN users on posts."userId" = users.id
+         WHERE  posts."id" > ${cursorValue} 
+		 ORDER BY posts."createdAt" DESC LIMIT ${20};
+        `)
+		
 		const {feed} = actualPosts
 		return expect(feed).to.eql(result.rows)	
 
@@ -42,7 +86,7 @@ describe('Fetch all posts', () => {
 			userId : user.id ,
 			postId : posts[0].id
 		})
-		const actualPosts = await fetchPosts(true)
+		const actualPosts = await fetchPosts({isFlagged : true})
         
 
 		const result = await db.query(`
@@ -69,7 +113,7 @@ describe('Fetch all posts', () => {
 			userId : user.id ,
 			postId : posts[0].id
 		})
-		const actualPosts = await fetchPosts(false)
+		const actualPosts = await fetchPosts({isFlagged : false})
         
 
 		const result = await db.query(`
@@ -83,6 +127,7 @@ describe('Fetch all posts', () => {
  		WHERE posts."flagsCount" = 0
 		ORDER BY "flagsCount" DESC LIMIT ${20};
         `)
+		
 		const {feed} = actualPosts
 		expect(feed).to.eql(result.rows)
 		feed.map(post => expect(post.flagsCount).to.equal(0))	
